d0372356fd078d86bfe1a9519ea234702283f46a
ZeKermet <158208383+ZeKermet@users.noreply.github.com>
2024-11-20 00:47:53 -0500

    Add files via upload

---
+++ skittlegen/main.js
@@ -0,0 +1,9 @@
+const colorsList = ['red', 'orange', 'yellow', 'green', 'purple'];
+
+const skittle = document.getElementById('circle');
+const button = document.getElementById('select');
+
+button.addEventListener('click', () => {
+  let colorPicker = Math.floor(Math.random() * colorsList.length) ;
+  skittle.style.background = colorsList[colorPicker];
+});
