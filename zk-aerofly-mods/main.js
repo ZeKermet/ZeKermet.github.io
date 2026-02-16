d1e40784ed1728482e64b56986832c24ace9f32d
ZeKermet <158208383+ZeKermet@users.noreply.github.com>
2026-02-16 16:03:32 -0500

    Add files via upload

---
+++ zk-aerofly-mods/main.js
@@ -0,0 +1,8 @@
+const GeneralLinks = {
+    getYoutubeChannel() {
+        return "https://www.youtube.com/@ZeKermet";
+    },
+    getAFSCDiscordInvite() {
+        return "https://discord.gg/4Z4Yjk5H";
+    }
+}
