d5f9405a99e77bc2e396fa8b2b0bc26cacc7b2ac
ZeKermet <158208383+ZeKermet@users.noreply.github.com>
2026-03-06 14:23:41 -0500

    Update ModsData.js

--- zk-aerofly-mods/resources/js/database/ModsData.js
+++ zk-aerofly-mods/resources/js/database/ModsData.js
@@ -9,7 +9,7 @@ function ModsData() {
             datePublished: "December 29, 2025",
             lastUpdated: "March 6, 2026",
             tags: ["b777", "b777-300er", "b777-200f", "Camera", "Shake", "Sounds", "Livery", "Boeing"],
-            articleImage: "b777_mod_old_emirates.png",
+            articleImage: "b777_lr_liv_1.png",
             content: [
                 {
                     type: "subheader",
@@ -839,4 +839,5 @@ function ModsData() {
     ];
 }
 */
+
 
